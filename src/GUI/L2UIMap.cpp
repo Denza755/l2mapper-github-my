c7c023fedad3cfd632d03612846ba86bfc7863d7
Thatgun <robbnzn@gmail.com>
2026-07-09 03:03:45 +0300

    1

--- src/GUI/L2UIMap.cpp
+++ src/GUI/L2UIMap.cpp
@@ -20,7 +20,6 @@ void L2UIMap::Init()
 	ui_mapWnd->setCaption(L"Map");
 	ui_mapWnd->eventWindowButtonPressed += MyGUI::newDelegate(this, &L2UIMap::onMapWindowClose);
 	ui_mapView = ui_mapWnd->createWidget<MyGUI::ScrollView>("ScrollView", 0, 0, 500, 400, MyGUI::Align::Stretch, "MapView");
-	//ui_mapView->setAlign(MyGUI::Align::HStretch | MyGUI::Align::VStretch);
 	ui_mapView->setVisibleVScroll(false);
 	ui_mapView->setVisibleHScroll(false);
 	ui_mapView->setCanvasSize(2796, 2650);
@@ -31,42 +30,7 @@ void L2UIMap::Init()
 	ui_mapView->setViewOffset(MyGUI::IntPoint(-1443, -1745));
 
 	char buf[256];
-	char *tbuf;
-
-/*
-	ui_mapTiles = new MyGUI::ImageBox**[MAP_TILES_X];
-	tileStates = new int16*[MAP_TILES_X];
-	for(int x = 10; x < 30; x++)
-	{
-		ui_mapTiles[x] = new MyGUI::ImageBox *[MAP_TILES_Y];
-		memset(ui_mapTiles[x], 0, MAP_TILES_Y * sizeof(MyGUI::ImageBox*));
-		tileStates[x] = new int16[MAP_TILES_Y];
-		memset(tileStates[x], 0, MAP_TILES_Y * sizeof(int16));
-		for(int y = 10; y < MAP_TILES_Y; y++)
-		{
-			sprintf(buf, "%d_%d", x, y);
-			//if(UPackageMgr.GetUPackage(buf))
-			//{
-				float tileWidth = 164;
-				float tileHeight = 162;
-				MyGUI::ImageBox *ui_mapTile_temp = ui_mapView->createWidget<MyGUI::ImageBox>("ImageBox", 820 + (x - 15) * tileWidth, 2440 + (y - 25) * tileHeight, tileWidth, tileHeight, MyGUI::Align::Default);
-				//ui_mapTile_temp->setImageTexture("textures/mapTile.png");
-				tbuf = new char[3];
-				sprintf(tbuf, "%d", x);
-				ui_mapTile_temp->setUserString("map_x", tbuf);
-				tbuf = new char[3];
-				sprintf(tbuf, "%d", y);
-				ui_mapTile_temp->setUserString("map_y", tbuf);
-				ui_mapTile_temp->eventMouseButtonPressed += MyGUI::newDelegate(this, &L2UIMap::onMapMouseDown);
-				ui_mapTile_temp->eventMouseButtonReleased += MyGUI::newDelegate(this, &L2UIMap::onMapMouseUp);
-				//ui_mapTile_temp->eventMouseButtonClick += MyGUI::newDelegate(this, &L2UIMap::onMapMouseClick);
-				ui_mapTiles[x][y] = ui_mapTile_temp;
-				setTileState(x, y, 0);
-			//}
-		}
-	}
-*/
-
+	char* tbuf;
 	ui_mapTiles = new MyGUI::ImageBox * *[MAP_TILES_X];
 	tileStates = new int16 * [MAP_TILES_X];
 
@@ -74,26 +38,34 @@ void L2UIMap::Init()
 	{
 		ui_mapTiles[x] = new MyGUI::ImageBox * [MAP_TILES_Y];
 		memset(ui_mapTiles[x], 0, MAP_TILES_Y * sizeof(MyGUI::ImageBox*));
+
+		// ИСПРАВЛЕНИЕ: Выделяем память под массив состояний ВСЕГДА, до проверки файла
 		tileStates[x] = new int16[MAP_TILES_Y];
 		memset(tileStates[x], 0, MAP_TILES_Y * sizeof(int16));
+
 		for (int y = 0; y < MAP_TILES_Y; y++)
 		{
-			// МЫ УБРАЛИ ПРОВЕРКУ ЗДЕСЬ, ЧТОБЫ СЕТКА РИСОВАЛАСЬ ВСЕГДА
-			float tileWidth = 164;
-			float tileHeight = 162;
-			MyGUI::ImageBox* ui_mapTile_temp = ui_mapView->createWidget<MyGUI::ImageBox>("ImageBox", 820 + (x - 15) * tileWidth, 2440 + (y - 25) * tileHeight, tileWidth, tileHeight, MyGUI::Align::Default);
+			sprintf(buf, "%d_%d", x, y);
+			if (UPackageMgr.GetUPackage(buf))
+			{
+				float tileWidth = 164;
+				float tileHeight = 162;
+				MyGUI::ImageBox* ui_mapTile_temp = ui_mapView->createWidget<MyGUI::ImageBox>("ImageBox", 820 + (x - 15) * tileWidth, 2440 + (y - 25) * tileHeight, tileWidth, tileHeight, MyGUI::Align::Default);
 
-			tbuf = new char[3];
-			sprintf(tbuf, "%d", x);
-			ui_mapTile_temp->setUserString("map_x", tbuf);
-			tbuf = new char[3];
-			sprintf(tbuf, "%d", y);
-			ui_mapTile_temp->setUserString("map_y", tbuf);
-			ui_mapTile_temp->eventMouseButtonPressed += MyGUI::newDelegate(this, &L2UIMap::onMapMouseDown);
-			ui_mapTile_temp->eventMouseButtonReleased += MyGUI::newDelegate(this, &L2UIMap::onMapMouseUp);
+				tbuf = new char[32]; // Безопасное выделение буфера под строку координаты
+				sprintf(tbuf, "%d", x);
+				ui_mapTile_temp->setUserString("map_x", tbuf);
 
-			ui_mapTiles[x][y] = ui_mapTile_temp;
-			setTileState(x, y, 0);
+				tbuf = new char[32]; // Безопасное выделение буфера под строку координаты
+				sprintf(tbuf, "%d", y);
+				ui_mapTile_temp->setUserString("map_y", tbuf);
+
+				ui_mapTile_temp->eventMouseButtonPressed += MyGUI::newDelegate(this, &L2UIMap::onMapMouseDown);
+				ui_mapTile_temp->eventMouseButtonReleased += MyGUI::newDelegate(this, &L2UIMap::onMapMouseUp);
+
+				ui_mapTiles[x][y] = ui_mapTile_temp;
+				setTileState(x, y, 0);
+			}
 		}
 	}
 
@@ -269,19 +241,24 @@ void L2UIMap::onMapWindowClose(MyGUI::Window* sender, const std::string& evt)
 
 void L2UIMap::onMapTileLoadClick(MyGUI::Widget* sender)
 {
-	jfArray<L2MapTileInfo*, uint16> *tiles = new jfArray<L2MapTileInfo*, uint16>();
-	for(uint16 x = 10; x < MAP_TILES_X; x++)
+	jfArray<L2MapTileInfo*, uint16>* tiles = new jfArray<L2MapTileInfo*, uint16>();
+
+	for (uint16 x = 0; x < MAP_TILES_X; x++)
 	{
-		for(uint16 y = 10; y < MAP_TILES_Y; y++)
+		for (uint16 y = 0; y < MAP_TILES_Y; y++)
 		{
+			// ИСПРАВЛЕНИЕ: Добавлена проверка на выделение памяти, чтобы исключить чтение пустых ячеек
+			if (!tileStates || !tileStates[x])
+				continue;
+
 			uint16 tileState = getTileState(x, y);
-			if((tileState & L2UIMTS_SELECTED) && !(getTileState(x, y) & L2UIMTS_ACTIVE))
+			if ((tileState & L2UIMTS_SELECTED) && !(tileState & L2UIMTS_ACTIVE))
 			{
-				L2MapTileInfo *tileInfo = new L2MapTileInfo();
+				L2MapTileInfo* tileInfo = new L2MapTileInfo();
 				tileInfo->map_x = x;
 				tileInfo->map_y = y;
 				tiles->add(tileInfo);
-				//g_geo.Load(x, y);
+				g_geo.Load(x, y);
 				setTileState(x, y, tileState | L2UIMTS_LOADED | L2UIMTS_ACTIVE);
 			}
 			setTileState(x, y, getTileState(x, y) & ~L2UIMTS_SELECTED);
@@ -293,7 +270,6 @@ void L2UIMap::onMapTileLoadClick(MyGUI::Widget* sender)
 
 	g_ui.getL2BusyScreen()->setMessage(L"Loading tiles...");
 	g_ui.getL2BusyScreen()->setVisible(true);
-	//setVisible(false);
 	g_levelMgr.loadTiles(tiles);
 }
 
@@ -320,6 +296,10 @@ void L2UIMap::onMapTileHideClick(MyGUI::Widget* sender)
 
 int16 L2UIMap::getTileState(int x, int y)
 {
+	// ЗАЩИТА: Если структура массивов не проинициализирована, возвращаем 0 вместо падения
+	if (!tileStates || !tileStates[x])
+		return 0;
+
 	return tileStates[x][y];
 }
 
